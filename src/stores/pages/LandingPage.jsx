4bdec7835ca590f63ba49096e634d462a0b81693
VictorAbhishiek <victorabhishiek@gmail.com>
2024-12-18 19:26:00 +0530

    second commit

--- src/stores/pages/LandingPage.jsx
+++ src/stores/pages/LandingPage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Products from "../components/Products";
-import "./App.jsx";
 const LandingPage = () => {
     return ( 
         <div>

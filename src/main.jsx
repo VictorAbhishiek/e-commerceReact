68e368fc43c310e05b23b97b06225721cb34a7ea
VictorAbhishiek <victorabhishiek@gmail.com>
2024-12-18 15:21:43 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,6 @@ createRoot(document.getElementById('root')).render(
     <Cartprovider>
       <App />
     </Cartprovider> 
-  </StrictMode>,
+  </StrictMode>
   </BrowserRouter> 
 )
--- src/stores/components/Login.jsx
+++ src/stores/components/Login.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Navbar from "./Navbar";
 
 const Login = ({onLogin}) => {
     const [email, setEmail] = useState('');
